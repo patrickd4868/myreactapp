e6ff5f2cc730f2f970133274fe87fb5a7b3ac8af
patrickd4868 <patrickd4868@gmail.com>
2023-06-13 20:46:42 -0400

    Side nav menu adjusted and collapsable

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { FiMenu, FiX } from 'react-icons/fi'
 import Home from './Pages/Home';
 import About from './Pages/About';
 
 function App() {
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <Router>
       <div className="App">
-        <nav className="sidebar">
+        
+       
+
+        <nav className={`sidebar ${isNavOpen ? 'open' : ''}`}>
+          <button className="menu-toggle" onClick={toggleNav}>
+                <FiX/>
+              </button>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={toggleNav}>Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={toggleNav}>About</Link>
             </li>
           </ul>
         </nav>
 
-        <div className="content">
-          <Routes>
+        <div className={`content ${isNavOpen ? 'nav-open' : ''}`}>
+          {!isNavOpen && (
+            <button className="menu-toggle" onClick={toggleNav}>
+              <FiMenu />
+            </button>
+          )}
+
+              <Routes>
             <Route path="/" element={<Home/>}/> 
             <Route path="/about" element={<About/>}/>
           </Routes>
-        </div>
+
+
+          </div>  
+        
         
       
       </div>      
